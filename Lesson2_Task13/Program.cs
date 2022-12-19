23b8bb4cefd25b3a75dd0051f64c411eb8ad36ee
Ilippy <Ilippyk@gmail.com>
2022-12-19 19:58:04 +0300

    add Task 13 project

---
+++ Lesson2_Task13/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 13: 
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679[2] -> 6
+// -35 -> третьей цифры нет
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        // string stringNumber = Console.ReadLine().Trim;
+        // ShowThirdDigit(stringNumber);
+        int number = EnterNumber("Введите одно число");
+        ShowThirdDigit(number);
+         
+
+    }
+
+
+    #region Methods
+    static int EnterNumber(string str)
+    {
+        System.Console.WriteLine(str);
+        string stringNumber = Console.ReadLine()!.Trim();
+        int number;
+        while (!int.TryParse(stringNumber, out number))
+        {
+            Console.WriteLine("Ошибка! Введите одно целое число");
+            stringNumber = Console.ReadLine()!.Trim();
+        }
+        return number;
+    }
+
+    static void ShowThirdDigit(int number){
+        int increment = number < 0 ? 0 : 1;
+        string str = number.ToString();
+        System.Console.WriteLine(str.Length < 3 + increment ? $"{str} -> третьей цифры нет" : 
+            $"{str} -> {str[2 + increment]}");
+    }
+
+    #endregion
+}
