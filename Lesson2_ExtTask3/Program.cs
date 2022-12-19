42f4db6b71604495ad1c249cef0373c392ffc674
Ilippy <Ilippyk@gmail.com>
2022-12-19 21:59:19 +0300

    add Extra Task 3 project

---
+++ Lesson2_ExtTask3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Генерируются два случайных целых числа c и d в интервале [-10 – 10].
+// Если оба числа четные, выводится их частное, если оба нечетные - их сумма,
+// если одно число четное, а другое нечетное, выводится их произведение ( if ),
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Random random = new Random();
+        int c = random.Next(-10, 11);
+        int d = random.Next(-10, 11);
+
+        if (c % 2 == 0 && d % 2 == 0)
+            System.Console.WriteLine(c > d ? $"{c} / {d} = {c / d}" :
+            $"{d} / {c} = {c / d}");
+        else if (c % 2 == 1 && d % 2 == 1)
+            System.Console.WriteLine($"{c} + {d} = {c + d}");
+        else System.Console.WriteLine($"{c} * {d} = {c * d}");
+
+    }
+}
