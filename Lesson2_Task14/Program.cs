919a8dd36e06cc82d93ef745387e5af0fa47c4f7
Ilippy <Ilippyk@gmail.com>
2022-12-19 18:43:07 +0300

    add Task 14 project

---
+++ Lesson2_Task14/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 
+// 7 и 23.
+
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+int number = new Random().Next(0, 1000);
+System.Console.WriteLine($"Ранодомное число равно {number}");
+
+if (number % 7 == 0 && number % 23 == 0) System.Console.WriteLine($"{number} -> да");
+else System.Console.WriteLine($"{number} -> нет");
+System.Console.WriteLine($"Число {number} делится на 7 с остатком {number % 7}");
+System.Console.WriteLine($"Число {number} делится на 23 с остатком {number % 23}");
+
+// for (int i = 23; i < 1000; i+=23)   // 161 322 483 644 805 966
+// {
+//     if (i % 7 == 0) System.Console.Write($"{i} ");
+// }  
