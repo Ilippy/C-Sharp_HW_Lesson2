8380a4a0eb416adfd8073f3d7be3e7426641e2c8
Ilippy <Ilippyk@gmail.com>
2022-12-22 01:37:53 +0300

    add Extra Task 20

---
+++ Lesson2_ExtTask20/Program.cs
@@ -0,0 +1,39 @@
+﻿// Ежедневно количество бактерий увеличивается на p процентов.
+// Через сколько дней количество бактерий увеличится в n раз (ввод p и n ).
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        double p = EnterNumber("Введите количество увеличения бактерий каждый день в процентах");
+        double n = EnterNumber("Введите в сколько раз нужно увеличить бактерии");
+
+        ShowBacteriaPerDay(p, n);
+    }
+
+    static double EnterNumber(string str)
+    {
+        System.Console.WriteLine(str);
+        double number;
+        string stringNumber = Console.ReadLine()!.Trim();
+        while (!double.TryParse(stringNumber, out number))
+        {
+            Console.WriteLine($"Ошибка! {str}");
+            stringNumber = Console.ReadLine()!.Trim();
+        }
+        return number;
+    }
+
+    static void ShowBacteriaPerDay(double p, double n)
+    {
+        double bacteria = 1, res = 1;
+        int i = 1;
+        while (res <= bacteria * n)
+        {
+            res += bacteria * p / 100; // dunno  bacteria * p / 100    or   res * p / 100
+            System.Console.WriteLine($"День {i}, бактерии {Math.Round(res, 2)}");
+            i++;
+        }
+        System.Console.WriteLine($"Нужно {i} дней, чтобы увеличить количество бактерий в {n} раз с шагом {p}%");
+    }
+}
