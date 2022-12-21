c193ccf20a71b09e8c2423c5992c90b2fab3628c
Ilippy <Ilippyk@gmail.com>
2022-12-21 22:37:10 +0300

    add Extra Task 13 project

---
+++ Lesson2_ExtTask13/Program.cs
@@ -0,0 +1,33 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int S = 90;
+        float b = EnterNumber("Введите расход бензина на 100 км");
+        float c = EnterNumber("Введите цену бензина за литр в рублях");
+
+        ShowSumGasCost(S, b, c);
+    }
+
+    static float EnterNumber(string str)
+    {
+        System.Console.WriteLine(str);
+        float number;
+        string stringNumber = Console.ReadLine()!.Trim();
+        while (!float.TryParse(stringNumber, out number))
+        {
+            Console.WriteLine($"Ошибка! {str}");
+            stringNumber = Console.ReadLine()!.Trim();
+        }
+        return number;
+    }
+
+    static void ShowSumGasCost(int S, float b, float c)
+    {
+        float result = 0;
+        for(int i = 10; i <= S; i= i + 10){
+            result += b/10 * c;
+            System.Console.WriteLine($"Вы проехали {i} км и вы потратили {result} рублей за бензин");
+        }
+    }
+}
